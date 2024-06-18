634acc501c09605794cf73afe32bb14d207f40ab
ATATC <futerry@outlook.com>
2024-06-19 07:30:45 +0800

    Bug fixed: unmatched image size.

--- prepare_swin_unet.py
+++ prepare_swin_unet.py
@@ -18,18 +18,18 @@ def remove_alpha_channel(image: Image) -> Image:
 if __name__ == '__main__':
     for i in range(158):
         tiff_image = Image.open(f"data/imagesTr/img_{(n := str(i).zfill(3))}.tiff")
-        tiff_image.resize((224, 224))
+        tiff_image = tiff_image.resize((224, 224))
         tiff_image = remove_alpha_channel(tiff_image)
         tiff_image.save(f"raw/Dataset001_NAME1/imagesTr/case_{n}_0000.png", "PNG")
 
     for i in range(158, 231):
         jpg_image = Image.open(f"data/imagesTr/img_{(n := str(i).zfill(3))}.jpg")
-        jpg_image.resize((224, 224))
+        jpg_image = jpg_image.resize((224, 224))
         jpg_image = remove_alpha_channel(jpg_image)
         jpg_image.save(f"raw/Dataset001_NAME1/imagesTr/case_{n}_0000.png", "PNG")
 
     for i in range(231):
         label_image = Image.open(f"data/labelsTr/img_{(n := str(i).zfill(3))}_label.png")
-        label_image.resize((224, 224))
+        label_image = label_image.resize((224, 224))
         label_image = remove_alpha_channel(label_image)
         label_image.save(f"raw/Dataset001_NAME1/labelsTr/case_{n}.png", "PNG")
