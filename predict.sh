45cbc3ca17d137dfc6096893698fc8d35b57787f
ATATC <futerry@outlook.com>
2024-06-20 19:01:17 +0800

    Bug fixed.

--- predict.sh
+++ predict.sh
@@ -3,4 +3,4 @@
 export nnUNet_raw="/Users/atatc/PycharmProjects/ML-Quiz/raw"
 export nnUNet_preprocessed="/Users/atatc/PycharmProjects/ML-Quiz/preprocessed"
 export nnUNet_results="/Users/atatc/PycharmProjects/ML-Quiz/weights"
-nnUNetv2_predict -i imagesTs-Internal -o results-Internal -d 1 -c 2d -f all--save_probabilities -device cpu
+nnUNetv2_predict -i imagesTs-Internal -o results-Internal -d 1 -c 2d -f all --save_probabilities -device cpu
